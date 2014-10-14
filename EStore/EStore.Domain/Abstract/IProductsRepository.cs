fa32ffa922390d8fcfd76ee3fb834e98750daa5c
Displace <joshua.omundson@gmail.com>
2014-10-13 19:40:19 -0700

    Added Product Model and Repository
    Added the domain Model and the beginnings of the Product Repository

---
+++ EStore/EStore.Domain/Abstract/IProductsRepository.cs
@@ -0,0 +1,18 @@
+﻿//Name: IProductRepository.cs
+//Author: Joshua Omundson
+//Date: 10.13.2014
+//Abstract: This is the repository for the Product object.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EStore.Domain.Entities;
+
+namespace EStore.Domain.Abstract
+{
+    public interface IProductsRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
---
+++ EStore/EStore.Domain/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿//Name: Product.cs
+//Author: Joshua Omundson
+//Date: 10.13.2014
+//Abstract: This is a POCO for a Product.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStore.Domain.Entities
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string  Name { get; set; }
+        public string  Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
