77af6c6280b138ccb18aba81dfea08f99736a0c8
Displace <joshua.omundson@gmail.com>
2014-10-15 21:38:41 -0700

    Changed default routing
    Changed the default routing to use the Product Controller and List View

--- EStore/EStore.WebUI/App_Start/RouteConfig.cs
+++ EStore/EStore.WebUI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EStore.WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
             );
         }
     }
