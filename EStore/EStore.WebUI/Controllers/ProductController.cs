50bcf271724072c4910f6504f23b64f04cde8d61
Displace <joshua.omundson@gmail.com>
2014-10-15 21:35:50 -0700

    Added Product Controller and List View
    Added the product controller and a basic list view

---
+++ EStore/EStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EStore.Domain.Abstract;
+using EStore.Domain.Entities;
+
+namespace EStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+
+        public ProductController(IProductsRepository repo)
+        {
+            this.repository = repo;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+      
+	}
+}
