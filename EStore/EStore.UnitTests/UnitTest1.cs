922bc598096522794998902a9448bffd492ea966
Displace <joshua.omundson@gmail.com>
2014-10-18 12:20:09 -0700

    Added Pagination Logic
    Updated Product controller to handle pagination.  Also set up Unit Test

--- EStore/EStore.UnitTests/UnitTest1.cs
+++ EStore/EStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using EStore.Domain.Abstract;
+using EStore.Domain.Entities;
+using EStore.WebUI.Controllers;
 
 namespace EStore.UnitTests
 {
@@ -7,8 +13,30 @@ namespace EStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            //Arrange
+            Mock<IProductsRepository> repo = new Mock<IProductsRepository>();
+            repo.Setup(m => m.Products).Returns(new Product[]
+                {
+                    new Product {ProductID = 1, Name = "P1"},
+                    new Product {ProductID = 2, Name = "P2"},
+                    new Product {ProductID = 3, Name = "P3"},
+                    new Product {ProductID = 4, Name = "P4"},
+                    new Product {ProductID = 5, Name = "P5"}
+                });
+
+            ProductController controller = new ProductController(repo.Object);
+            controller.PageSize = 3;
+
+            //Act
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+
+            //Assert
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
         }
     }
 }
--- EStore/EStore.WebUI/Controllers/ProductController.cs
+++ EStore/EStore.WebUI/Controllers/ProductController.cs
@@ -11,15 +11,19 @@ namespace EStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductsRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductsRepository repo)
         {
             this.repository = repo;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            //return View(repository.Products);
+            
+            //This handles the paging request.  
+            return View(repository.Products.OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize));
         }
       
 	}
