18e9ff9b33b2eb919aa59ed172846106e4dd1452
Displace <joshua.omundson@gmail.com>
2014-11-09 20:33:24 -0700

    Added Cart Controller and Shopping Cart page
    Added Cart controller to manage shopping cart and Shopping Cart page to
    show all items in cart.

---
+++ EStore/EStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EStore.Domain.Entities;
+using EStore.Domain.Abstract;
+using EStore.WebUI.Models;
+
+namespace EStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductsRepository repository;
+
+        public CartController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+
+        //Get
+        public ViewResult Index(string returnUrl)
+        {
+            return View (new CartIndexViewModel{
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        //Adds an item to the cart and shows shopping cart page.
+        [HttpPost]
+        public RedirectToRouteResult AddToCart(int productID, string returnUrl)
+        {
+            //Finds the product by the product id.
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
+
+            //Integrity check.  If the item exists then add one to the cart, otherwise do nothing.
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+
+            //Send user to the shopping cart with a return url of where they were when they
+            //clicked the "Add to cart" button.
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        //Removes an item from the cart and shows the shopping cart page.
+        [HttpPost]
+        public RedirectToRouteResult RemoveFromCart(int productID, string returnUrl)
+        {
+            //Finds the product by the product id.
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
+
+            //Integrity check.  If the item exists the remove, otherwise do nothing.
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+
+            //Sends user to the shopping cart page with a return url of where they were when they
+            //clicked the "remove from cart" button.
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        //Gets current cart from session state.
+        private Cart GetCart()
+        {
+            //Gets cart from session state
+            Cart cart = (Cart)Session["Cart"];
+
+            //If there is no cart object in session state create one and 
+            //save it to session state.
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+	}
+}
---
+++ EStore/EStore.WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EStore.Domain.Entities;
+
+namespace EStore.WebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
