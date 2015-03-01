980d733f3ef9328cea567b877639edad98ac7277
Displace <joshua.omundson@gmail.com>
2015-03-01 10:14:43 -0700

    Added Order Processing class

---
+++ EStore/EStore.Domain/Abstract/IOrderProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EStore.Domain.Entities;
+
+namespace EStore.Domain.Abstract
+{
+    public interface IOrderProcessor
+    {
+        void ProcessOrder(Cart cart, ShippingDetails shippingDetails);
+    }
+}
---
+++ EStore/EStore.Domain/Concrete/EmailOrderProcessor.cs
@@ -0,0 +1,97 @@
+﻿//NOTE: If you set the emailSetting.WriteAsFile to true, the file will be written with the .eml extension,
+//which can be read by any text editor.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EStore.Domain.Abstract;
+using EStore.Domain.Entities;
+using System.Net.Mail;
+using System.Net;
+
+namespace EStore.Domain.Concrete
+{
+    public class EmailOrderProcessor : IOrderProcessor
+    {
+        private EmailSettings emailSettings;
+
+        //Constructor
+        public EmailOrderProcessor(EmailSettings settings)
+        {
+            emailSettings = settings;
+        }
+
+        public void ProcessOrder(Cart cart, ShippingDetails shippingInfo)
+        {
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.EnableSsl = emailSettings.UseSsl;
+                smtpClient.Host = emailSettings.ServerName;
+                smtpClient.Port = emailSettings.ServerPort;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(emailSettings.UserName, emailSettings.Password);
+
+                //If the order is flagged to write to a file, set file location information.
+                if (emailSettings.WriteAsFile)
+                {
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtpClient.PickupDirectoryLocation = emailSettings.FileLocation;
+                    smtpClient.EnableSsl = false;
+                }
+
+                //Start of the Order Proccessing email/file
+                StringBuilder body = new StringBuilder()
+                    .Append("A new order has been submitted")
+                    .AppendLine("---")
+                    .AppendLine("Items:");
+
+                //for each set of items show to quanity, name, and subtotal cost.
+                //and add it to the email/file body
+                foreach (var line in cart.Lines)
+                {
+                    var subtotal = line.Product.Price * line.Quantity;
+                    body.AppendFormat("{0} x {1} (subtotal: {2:c}", line.Quantity, line.Product.Name, subtotal);
+                }
+
+                //Adds total order information, plus shipping details to email/file body
+                body.AppendFormat("Total order value: {0:c}", cart.ComputeTotalValue())
+                    .AppendLine("---")
+                    .AppendLine("Ship to:")
+                    .AppendLine(shippingInfo.Name)
+                    .AppendLine(shippingInfo.Line1)
+                    .AppendLine(shippingInfo.Line2 ?? "")
+                    .AppendLine(shippingInfo.Line3 ?? "")
+                    .AppendLine(shippingInfo.City)
+                    .AppendLine(shippingInfo.State ?? "")
+                    .AppendLine(shippingInfo.Country)
+                    .AppendLine(shippingInfo.Zip)
+                    .AppendLine("---")
+                    .AppendFormat("Gift wrap: {0}", shippingInfo.GiftWrap ? "Yes" : "No");
+
+                //Creates a new mail message.
+                MailMessage mailMessage = new MailMessage(emailSettings.MailFromAddress, emailSettings.MailToAddress, "New Order Submitted!", body.ToString());
+                
+                if (emailSettings.WriteAsFile)
+                {
+                    mailMessage.BodyEncoding = Encoding.ASCII;
+                }
+
+                smtpClient.Send(mailMessage);
+            }
+        }
+    }
+
+    public class EmailSettings
+    {
+        public string MailToAddress = "orders@example.com";
+        public string MailFromAddress = "EStore@example.com";
+        public bool UseSsl = true;
+        public string UserName = "MySmtpUserName";
+        public string Password = "MySmtpPassword";
+        public string ServerName = "smtp.example.com";
+        public int ServerPort = 587;
+        public bool WriteAsFile = false;
+        public string FileLocation = @"c:\sports_store_emails";
+    }
+}
