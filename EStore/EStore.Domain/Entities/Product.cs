34b1b7387a917fb3fc11d55215722d906c6a4cbd
Displace <joshua.omundson@gmail.com>
2015-03-01 19:46:31 -0700

    Added DataAnnotations for Validation

--- EStore/EStore.Domain/Entities/Product.cs
+++ EStore/EStore.Domain/Entities/Product.cs
@@ -16,11 +16,19 @@ namespace EStore.Domain.Entities
     {
         [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product name")]
         public string  Name { get; set; }
         
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Please enter a description")]
         public string  Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please specify a Category")]
         public string Category { get; set; }
     }
 }
