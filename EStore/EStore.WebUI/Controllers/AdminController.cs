db68762cef64caf0e01cfae45392102e0ccd2ea5
Displace <joshua.omundson@gmail.com>
2015-03-01 17:27:27 -0700

    Created Admin Controller

---
+++ EStore/EStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EStore.Domain.Abstract;
+
+namespace EStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductsRepository repository;
+
+        public AdminController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+
+        //
+        // GET: /Admin/
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+	}
+}
